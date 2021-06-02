72f7560fdd9ea94e01e5ab0828a3b20312e8394e
niqdev <niqdev@users.noreply.github.com>
2021-06-02 22:31:52 +0100

    update git script

--- .github/scripts/telegram.sh
+++ .github/scripts/telegram.sh
@@ -11,10 +11,14 @@ gh --version
 TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
 OUTPUT_PATH="data/telegram-${TIMESTAMP}.json"
 
+# poll latest messages
+# TODO store offset
 curl -s "https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/getUpdates" | \
   jq --arg TELEGRAM_FROM_ID ${TELEGRAM_FROM_ID} '[ .result[] | select(.message.from.id==($TELEGRAM_FROM_ID|tonumber)) ] | map({"update_id": .update_id, "message_text": .message.text})' \
   > $OUTPUT_PATH
 
+# must be on a different branch than main
+git checkout -b $OUTPUT_PATH
 gh pr create --title "[TELEGRAM] $OUTPUT_PATH"
 
 echo "[-] telegram"
