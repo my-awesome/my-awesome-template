8fa6f00836ba75b14db5bc608872b6bbdec60c60
niqdev <niqdev@users.noreply.github.com>
2021-06-02 22:24:26 +0100

    update script

--- .github/scripts/telegram.sh
+++ .github/scripts/telegram.sh
@@ -6,7 +6,15 @@ echo "[+] telegram"
 
 curl --version
 jq --version
+gh --version
 
-curl -s "https://api.telegram.org/bot$TELEGRAM_API_TOKEN/getUpdates" | jq --arg TELEGRAM_FROM_ID $TELEGRAM_FROM_ID '[ .result[] | select(.message.from.id==($TELEGRAM_FROM_ID|tonumber)) ] | map({"update_id": .update_id, "message_text": .message.text})'
+TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
+OUTPUT_PATH="data/telegram-${TIMESTAMP}.json"
+
+curl -s "https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/getUpdates" | \
+  jq --arg TELEGRAM_FROM_ID ${TELEGRAM_FROM_ID} '[ .result[] | select(.message.from.id==($TELEGRAM_FROM_ID|tonumber)) ] | map({"update_id": .update_id, "message_text": .message.text})' \
+  > $OUTPUT_PATH
+
+gh pr create --title "[TELEGRAM] $OUTPUT_PATH"
 
 echo "[-] telegram"
