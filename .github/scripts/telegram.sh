95a6e45cfe7a79c3813c69720d17cef4ee29e634
niqdev <niqdev@users.noreply.github.com>
2021-06-06 13:11:18 +0100

    remove properties

--- .github/scripts/telegram.sh
+++ .github/scripts/telegram.sh
@@ -8,50 +8,53 @@ echo "[+] telegram"
 curl --version
 jq --version
 
-##############################
-
-DATA_PATH="./data"
-PROPERTIES_PATH="${DATA_PATH}/telegram.properties"
-
-# ONLY for local testing
-#TELEGRAM_API_TOKEN=
-#TELEGRAM_FROM_ID=
-#source "${DATA_PATH}/telegram.secrets"
-
-OUTPUT_PATH="${DATA_PATH}/telegram.json"
-
-# "source" loads properties to env variables
-source ${PROPERTIES_PATH}
-API_URL=$api_url
-CURRENT_OFFSET=$offset
-
-echo "[*] OUTPUT_PATH=${OUTPUT_PATH}"
-echo "[*] PROPERTIES_PATH=${PROPERTIES_PATH}"
-echo "[*] API_URL=${API_URL}"
-echo "[*] CURRENT_OFFSET=${CURRENT_OFFSET}"
+# ONLY for local testing: "source" loads env variables
+source "./data/telegram.secrets"
 
 ##############################
 
-function build_query_params {
-  # check if offset exists and increase it by 1
-  [[ -z "${CURRENT_OFFSET}" ]] && echo "" || echo "?offset=$((${CURRENT_OFFSET} + 1))"
+# global param: <DATA_PATH>
+function count_messages {
+  echo $(cat ${DATA_PATH} | jq '. | length')
 }
 
+# global param: <DATA_PATH>
+function get_latest_offset {
+  # expected format: [] or [{"update_id":123, ...}]
+  # use "-r" to avoid printing quotes
+  echo $(cat ${DATA_PATH} | jq -r '. | last | .update_id // ""')
+}
+
+function build_query_params {
+  local OFFSET=$(get_latest_offset)
+
+  # check if offset exists and increase it by 1
+  [[ -z "${OFFSET}" ]] && echo "" || echo "?offset=$((${OFFSET} + 1))"
+}
+
+# global param: <TELEGRAM_API_TOKEN>
 function build_url {
   local OFFSET_PARAM=$(build_query_params)
 
-  echo "${API_URL}/bot${TELEGRAM_API_TOKEN}/getUpdates${OFFSET_PARAM}"
+  echo "https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/getUpdates${OFFSET_PARAM}"
 }
 
 function request_latest_messages {
   local REQUEST_URL=$(build_url)
 
-  # sample response
+  echo $(curl -s ${REQUEST_URL})
+}
+
+# global param: <TELEGRAM_FROM_ID>
+function validate_messages {
+  local RESPONSE=$(request_latest_messages)
+
+  # sample response: "text" field is optional
   # {
   #   "ok": true,
   #   "result": [
   #    {
-  #      "update_id": 60598210,
+  #      "update_id": 123,
   #      "message": {
   #        "message_id": 42,
   #        "from": {
@@ -71,61 +74,49 @@ function request_latest_messages {
   #    }
   #  ]
   # }
-  # use "-c" to have 1 line  
-  echo $(curl -s ${REQUEST_URL}) | jq -c --arg TELEGRAM_FROM_ID ${TELEGRAM_FROM_ID} \
-    '[ .result[] | select(.message.from.id==($TELEGRAM_FROM_ID|tonumber)) ] | map({"update_id": .update_id, "message_text": [ try(.message.text) catch "" | gsub("\\s+";"\n") | splits("\n")] })'
+  echo ${RESPONSE} | jq -c --arg TELEGRAM_FROM_ID ${TELEGRAM_FROM_ID} \
+    '[ .result[] | select(.message.from.id==($TELEGRAM_FROM_ID | tonumber)) ] |
+      map({
+        "update_id": .update_id,
+        "message_text": [ try(.message.text) catch "" | gsub("\\s+";"\n") | splits("\n") ]
+      })'
+}
+
+function parse_messages {
+  local MESSAGES=$(validate_messages)
+
+  # expected format: [] or [{"update_id":123,"message_text":["test"]}]
+
+  # TODO
+  echo $MESSAGES
 }
 
 function concat_messages {
   local VALUES=$1
-  local OLD_VALUES="$(cat ${OUTPUT_PATH} | jq '.')"
+  local OLD_VALUES="$(cat ${DATA_PATH} | jq '.')"
 
   # mandatory quotes on argjson value
   jq -n \
     --argjson OLD_MESSAGES "${OLD_VALUES}" \
     --argjson NEW_MESSAGES "${VALUES}" \
     '$OLD_MESSAGES + $NEW_MESSAGES' \
-    > ${OUTPUT_PATH}
-}
-
-function get_latest_offset {
-  # expected format: [{"update_id":123,"message_text":"hello"}]
-  local JSON_STRING=$1
-
-  # use "-r" to avoid printing quotes, hence string must be 1 line
-  echo ${JSON_STRING} | jq -r '. | last | .update_id // ""'
-}
-
-function update_offset {
-  local VALUE=$1
-
-  if [[ -z "${VALUE}" ]]; then
-    echo "[-] Offset NOT updated"
-  else
-    sed -i "s/^offset=.*/offset=${VALUE}/" ${PROPERTIES_PATH}
-    echo "[-] Offset updated"
-  fi
+    > ${DATA_PATH}
 }
 
 ##############################
 
 function main {
-  echo "[+] telegram"
+  echo "[*] DATA_PATH=${DATA_PATH}"
+  echo "[*] OFFSET=$(get_latest_offset)"
+  echo "[*] COUNT=$(count_messages)"
 
-  local MESSAGES=$(request_latest_messages)
+  local MESSAGES=$(parse_messages)
   echo -e "[*] MESSAGES=\n${MESSAGES}"
-
-  local OLD_COUNT=$(cat ${OUTPUT_PATH} | jq '. | length')
-  local NEW_COUNT=$(echo ${MESSAGES} | jq '. | length')
-  echo "[*] OLD_COUNT=${OLD_COUNT}"
-  echo "[*] NEW_COUNT=${NEW_COUNT}"
   
   concat_messages "${MESSAGES}"
 
-  local LATEST_OFFSET=$(get_latest_offset ${MESSAGES})
-  echo "[*] LATEST_OFFSET=${LATEST_OFFSET}"
-
-  update_offset "${LATEST_OFFSET}"
+  echo "[*] NEW_OFFSET=$(get_latest_offset)"
+  echo "[*] NEW_COUNT=$(count_messages)"
 }
 
 # TODO make paths configurable
